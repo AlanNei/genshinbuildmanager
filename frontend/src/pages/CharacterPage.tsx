a2787ea00be3a71aa6cb66a2223ef149684278a1
AlanNei <alanrespinozaz@gmail.com>
2026-04-09 00:25:42 -0600

    Agregue el header con uid a la pagina de personaje

--- frontend/src/pages/CharacterPage.tsx
+++ frontend/src/pages/CharacterPage.tsx
@@ -1,13 +1,16 @@
 import { useLocation } from "react-router-dom";
-import { CharacterHeader, CharacterStats, CharacterWeapon, CharacterArtifacts } from "../components";
+import { CharacterHeader, CharacterStats, CharacterWeapon, CharacterArtifacts, Header } from "../components";
 
 function CharacterPage() {
 
     const location = useLocation();
-    const {character} = location.state
+    const {character, uid} = location.state
 
     return(
         <div>
+            {/*Header*/}
+            <Header uid={uid} />
+
             {/*Informacion del personaje*/}
             <CharacterHeader character={character}/>
 
