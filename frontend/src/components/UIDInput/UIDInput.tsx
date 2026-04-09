1099805607b45e011b86c5c1ae9f4b5639ab2cf4
AlanNei <alanrespinozaz@gmail.com>
2026-04-09 00:24:15 -0600

    Agreuege informacion extra al componente, ahora se puede buscar con la tecla enter y mejore el css

--- frontend/src/components/UIDInput/UIDInput.tsx
+++ frontend/src/components/UIDInput/UIDInput.tsx
@@ -14,18 +14,32 @@ function UIDInput({ onSearch }: UIDInputProps) {
         onSearch(uid);
     }
 
+    const handleKeyDown = (e: React.KeyboardEvent)=>{
+        if (e.key === "Enter") handleSearch();
+    }
+
     return (
-        <div className="input-container">
-            <input 
-            type="text"
-            placeholder="Enter UID" 
-            value={uid}
-            onChange={(e) => setUid(e.target.value)}
-            />
-        
-        <button onClick={handleSearch}>
-            Search
-        </button>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div className="input-text">
+                Enter your Genshin Impact UID to start
+            </div>
+            <div className="input-container">
+                <input 
+                type="text"
+                placeholder="Enter UID..." 
+                value={uid}
+                onChange={(e) => setUid(e.target.value)}
+                onKeyDown={handleKeyDown}
+                />
+            
+            <button onClick={handleSearch}>
+                &#10095;
+            </button>
+            </div>
+            <div className="input-hint">
+                <span className="hint-icon">i</span> 
+                Find it in-game: Paimon Menu → bottom right corner
+            </div>
         </div>
     );
 }
