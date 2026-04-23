e55b4432a4a6a206b8f36b0705681fa99a11d2dc
AlanNei <alanrespinozaz@gmail.com>
2026-04-23 17:20:13 -0600

    elimine characterweapon del puente

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,7 +3,6 @@ export { default as PlayerProfile } from './PlayerProfile/PlayerProfile';
 export { default as CharacterList } from './CharacterList/CharacterList';
 export { default as CharacterHeader } from './CharacterHeader/CharacterHeader';
 export { default as CharacterStats } from './CharacterStats/CharacterStats';
-export { default as CharacterWeapon } from './CharacterWeapon/CharacterWeapon';
 export { default as CharacterArtifacts } from './CharacterArtifacts/CharacterArtifacts';
 export { default as Header } from './Header/Header';
 export { default as Loading } from './Loading/Loading';
